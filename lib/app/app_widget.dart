f2fe8ad42237e12d968077e8628a7f4070b32387
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-03-17 13:42:59 -0300

    REFACTOR: atualizando rotas;

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:litoral_delivery_parceiros/app/pages/product_details/product_details_page.dart';
 
 import 'core/provider/application_binding.dart';
 import 'core/ui/theme/theme_config.dart';
 import 'pages/auth/login/login_router.dart';
-import 'pages/auth/register/organization_form.dart';
+import 'pages/auth/register/register_router.dart';
+import 'pages/home/home_page.dart';
+import 'pages/product_registration/product_registration_page.dart';
 import 'pages/splash/splash_page.dart';
+import 'pages/welcome/welcome_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -21,9 +25,11 @@ class AppWidget extends StatelessWidget {
         routes: {
           '/': (context) => const SplashPage(),
           '/auth/login': (context) => LoginRouter.page,
-          '/auth/register': (context) => const OrganizationForm(),
-          // '/home': (context) => HomeRouter.page,
-          // '/productDetail': (context) => ProductDetailRouter.page,
+          '/auth/register': (context) => RegisterRouter.page,
+          '/welcome': (context) => const WelcomePage(),
+          '/registerProduct': (context) => const ProductRegistrationPage(),
+          '/home': (context) => const HomePage(),
+          '/productDetails': (context) => const ProductDetailsPage(),
           // '/order': (context) => OrderRouter.page,
           // '/order/completed': (context) => const OrderCompletedPage(),
         },
