cf78a84f02c8e7d6013dab3121e71de8c3c5acdd
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-03-17 13:02:29 -0300

    REFACTOR: add toMap();

--- lib/app/models/product_model.dart
+++ lib/app/models/product_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ProductModel {
   final String name;
   final double price;
@@ -7,4 +9,24 @@ class ProductModel {
     required this.price,
     required this.description,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'price': price,
+      'description': description,
+    };
+  }
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      name: map['name'] as String,
+      price: map['price'] as double,
+      description: map['description'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ProductModel.fromJson(String source) => ProductModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
