cdc4356452f8a7ce761fd3f5023d3fec27eca7c3
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-03-17 12:54:50 -0300

    REFACTOR: removendo extensão de controller e bloc;

--- lib/app/core/ui/base_state/base_state.dart
+++ lib/app/core/ui/base_state/base_state.dart
@@ -1,22 +1,18 @@
-// import 'package:bloc/bloc.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 
-// import '../helpers/loader_mixin.dart';
-// import '../helpers/messages_mixin.dart';
+import '../helpers/loader_mixin.dart';
+import '../helpers/messages_mixin.dart';
 
-// abstract class BaseState<T extends StatefulWidget, C extends BlocBase>
-//     extends State<T> with LoaderMixin, MessagesMixin {
-//       late final C controller; 
-
-//       @override
-//       void initState() {
-//         super.initState();
-//         controller = context.read<C>();
-//         WidgetsBinding.instance.addPostFrameCallback(((timeStamp) {
-//           onReady();
-//         }));
-//       }
-//       // método chamado quando a tela é contruída!
-//       void onReady(){} 
-//     }
+abstract class BaseState<T extends StatefulWidget> extends State<T>
+    with LoaderMixin, MessagesMixin {
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   controller = context.read<C>();
+  //   WidgetsBinding.instance.addPostFrameCallback(((timeStamp) {
+  //     onReady();
+  //   }));
+  // }
+  // // método chamado quando a tela é contruída!
+  // void onReady(){}
+}
