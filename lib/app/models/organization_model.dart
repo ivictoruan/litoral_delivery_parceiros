900d95b747a3eae45aa10303a139ef06dad537cf
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-03-17 12:59:16 -0300

    REFACTOR: add distribuidora* password e toString;

--- lib/app/models/organization_model.dart
+++ lib/app/models/organization_model.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'package:flutter/material.dart';
 
 import 'address_model.dart';
 import 'product_model.dart';
 
 enum OrganizationCategory {
+  distribuidora,
   lanchonete,
   restaurante,
   bar,
@@ -26,18 +29,26 @@ class OrganizationModel extends ChangeNotifier {
   List<String> openingDays;
   String openingTime;
   String closingTime;
+  String password;
 
-  OrganizationModel(
-      {this.email = '',
-      this.products = const [],
-      this.contactNumber = '',
-      this.name = '',
-      this.responsibleName = '',
-      required this.address,
-      this.category =
-          OrganizationCategory.outro, // e se for mais de uma categoria?
-      this.description = '',
-      this.openingTime = '',
-      this.closingTime = '',
-      this.openingDays = const []});
+  OrganizationModel({
+    this.email = '',
+    this.products = const [],
+    this.contactNumber = '',
+    this.name = '',
+    this.responsibleName = '',
+    required this.address,
+    this.category =
+        OrganizationCategory.outro, // e se for mais de uma categoria?
+    this.description = '',
+    this.openingTime = '',
+    this.closingTime = '',
+    this.openingDays = const [],
+    this.password = '',
+  });
+  
+  @override
+  String toString() {
+    return 'OrganizationModel(email: $email, name: $name, contactNumber: $contactNumber, responsibleName: $responsibleName, products: $products, address: $address, category: $category, description: $description, openingDays: $openingDays, openingTime: $openingTime, closingTime: $closingTime, password: $password)';
+  }
 }
