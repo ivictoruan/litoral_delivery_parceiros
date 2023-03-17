d7c8ec50448faa5284732095a9681a883292a469
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-03-17 13:29:10 -0300

    REFACTOR: add função de login + status;

--- lib/app/pages/auth/login/controller/login_controller.dart
+++ lib/app/pages/auth/login/controller/login_controller.dart
@@ -1,42 +1,37 @@
-// import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:match/match.dart';
 
-// import 'package:bloc/bloc.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+part 'login_controller.g.dart';
 
-// import '../../../../core/exceptions/unauthorized_exception.dart';
-// import '../../../../models/auth_model.dart';
-// import '../../../../repositories/auth/auth_repository.dart';
-// import 'login_state.dart';
+@match
+enum LoginStatus {
+  initial,
+  success,
+  login,
+  loading,
+  loginError,
+  error,
+}
 
-// class LoginController extends Cubit<LoginState> {
-//   final AuthRepository _authRepository;
+class LoginController extends ChangeNotifier {
+  String email = "victor@gmail.com";
+  String password = "123456";
 
-//   LoginController(this._authRepository) : super(const LoginState.initial());
+  LoginStatus status = LoginStatus.initial;
 
-//   Future<void> login(String email, String password) async {
-//     try {
-//       emit(state.copyWith(status: LoginStatus.login));
-//       final AuthModel authModel = await _authRepository.login(email, password);
-//       emit(state.copyWith(status: LoginStatus.success));
-//       final sp = await SharedPreferences.getInstance();
-//       sp.setString("accessToken", authModel.accessToken);
-//       sp.setString("refreshToken", authModel.refreshToken);
-//     } on UnauthorizedException catch (e, s) {
-//       log("Login e senha inválidos!", error: e, stackTrace: s);
-//       emit(
-//         state.copyWith(
-//           status: LoginStatus.loginError,
-//           errorMessage: "Login ou senha inválidos!",
-//         ),
-//       );
-//     } catch (e, s) {
-//       log("Erro ao registrar usuário", error: e, stackTrace: s);
-//       emit(
-//         state.copyWith(
-//           status: LoginStatus.error,
-//           errorMessage: "Erro ao realizar login!",
-//         ),
-//       );
-//     }
-//   }
-// }
+  LoginController({
+    this.status = LoginStatus.initial,
+  });
+
+  void login(String email, String password) {
+    status = LoginStatus.loading;
+
+    if (email == this.email && password == this.password) {
+      status = LoginStatus.success;
+      notifyListeners();
+    } else {
+      status = LoginStatus.error;
+      notifyListeners();
+    }
+  }
+}
